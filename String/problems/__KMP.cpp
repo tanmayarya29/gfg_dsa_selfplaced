c2ad01b6e3a661afb4b2f09beff02fffba3f6cb3
tanmayarya29 <55969671+tanmayarya29@users.noreply.github.com>
2021-07-07 02:54:07 +0530

    Create __KMP.cpp

---
+++ String/problems/__KMP.cpp
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// C++ program for implementation of KMP pattern searching 
+// algorithm 
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+//Function to fill lps[] for given patttern pat[0..M-1].
+void computeLPSArray(string pat, int M, int* lps) 
+{ 
+	// Your code here
+	 // length of the previous longest prefix suffix
+    int len = 0;
+  
+    lps[0] = 0; // lps[0] is always 0
+  
+    // the loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0) {
+                len = lps[len - 1];
+  
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+} 
+
+//Function to check if the pattern exists in the string or not.
+bool KMPSearch(string pat, string txt) 
+{
+    // Your code here
+     int M = pat.length();
+    int N = txt.length();
+  
+    // create lps[] that will hold the longest prefix suffix
+    // values for pattern
+    int lps[M];
+  
+    // Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps);
+  
+    int i = 0; // index for txt[]
+    int j = 0; // index for pat[]
+    while (i < N) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+  
+        if (j == M) {
+            return 1;
+            j = lps[j - 1];
+        }
+  
+        // mismatch after j matches
+        else if (i < N && pat[j] != txt[i]) {
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }return 0;
+    
+}
+
+// { Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{ 
+    int t;
+    std::cin >> t;
+    
+    while(t--){
+        string s, pat;
+        cin >> s >> pat;
+        if(KMPSearch(pat, s)){
+            cout << "Yes" << endl;
+        }
+        else cout << "No" << endl;
+    }
+    
+	return 0;
+} 
+  // } Driver Code Ends
